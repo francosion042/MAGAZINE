a319832bfa11239220286228ddd6eedf7999b744
Nwobodo Anthony <anthonynwobodo7@gmail.com>
2019-07-25 17:50:55 +0100

    Update Posts.js

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -14,19 +14,19 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
   return loading ? (
     <Spinner />
   ) : (
-    <Fragment>
-      <h1 className='large text-primary'>Posts</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Welcome to the community
+      <Fragment>
+        <h1 className='large text-primary'>Posts/Views</h1>
+        <p className='lead'>
+          <i className='fas fa-user' /> Welcome to the community
       </p>
-      <PostForm />
-      <div className='posts'>
-        {posts.map(post => (
-          <PostItem key={post._id} post={post} />
-        ))}
-      </div>
-    </Fragment>
-  );
+        <PostForm />
+        <div className='posts'>
+          {posts.map(post => (
+            <PostItem key={post._id} post={post} />
+          ))}
+        </div>
+      </Fragment>
+    );
 };
 
 Posts.propTypes = {
