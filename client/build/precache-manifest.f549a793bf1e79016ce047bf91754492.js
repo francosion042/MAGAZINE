034998562b1f247f2b5d4bcda01a023b5821c07e
Nwobodo Anthony <anthonynwobodo7@gmail.com>
2019-07-29 02:55:17 +0100

    changed

---
+++ client/build/precache-manifest.f549a793bf1e79016ce047bf91754492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464c53df0fecfd0da08f525c679c2b09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661a405db36bda7cbc4f",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "f8a9518b0089bab571f3",
+    "url": "/static/js/2.df383d2a.chunk.js"
+  },
+  {
+    "revision": "661a405db36bda7cbc4f",
+    "url": "/static/js/main.70ebb4dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -5,36 +5,37 @@ import { connect } from 'react-redux';
 import Moment from 'react-moment';
 import { deleteComment } from '../../actions/post';
 
+
 const CommentItem = ({
   postId,
   comment: { _id, text, name, avatar, user, date },
   auth,
   deleteComment
 }) => (
-  <div className='post bg-white p-1 my-1'>
-    <div>
-      <Link to={`/profile/${user}`}>
-        <img className='round-img' src={avatar} alt='' />
-        <h4>{name}</h4>
-      </Link>
+    <div className='post bg-white p-1 my-1'>
+      <div>
+        <Link to={`/profile/${user}`}>
+          <img className='round-img' src={avatar} alt='' />
+          <h4>{name}</h4>
+        </Link>
+      </div>
+      <div>
+        <p className='my-1'>{text}</p>
+        <p className='post-date'>
+          Commented on <Moment format='YYYY/MM/DD By h-MM-SS-A'>{date}</Moment>
+        </p>
+        {!auth.loading && user === auth.user._id && (
+          <button
+            onClick={() => deleteComment(postId, _id)}
+            type='button'
+            className='btn btn-danger'
+          >
+            <i className='fas fa-times' />
+          </button>
+        )}
+      </div>
     </div>
-    <div>
-      <p className='my-1'>{text}</p>
-      <p className='post-date'>
-        Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
-      </p>
-      {!auth.loading && user === auth.user._id && (
-        <button
-          onClick={() => deleteComment(postId, _id)}
-          type='button'
-          className='btn btn-danger'
-        >
-          <i className='fas fa-times' />
-        </button>
-      )}
-    </div>
-  </div>
-);
+  );
 
 CommentItem.propTypes = {
   postId: PropTypes.string.isRequired,
@@ -51,3 +52,5 @@ export default connect(
   mapStateToProps,
   { deleteComment }
 )(CommentItem);
+
+
